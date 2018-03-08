c97a45f60db480dcb82f5a59ec8f8da16497e44f
leon yue <4940748@qq.com>
2018-03-08 09:47:52 +0800

    save state

--- PhotoManagement/ViewController.m
+++ PhotoManagement/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    ALAuthorizationStatus authorStatus=[ALAssetsLibrary authorizationStatus];
+    
+    if (authorStatus != ALAuthorizationStatusAuthorized) {
+        return;
+    }
+    ALAssetsLibrary *assetsLibrary=[[ALAssetsLibrary alloc]init];
+    //通过URL地址获取在相册中ALAsset，该方法是异步的
+    [assetsLibrary assetForURL:[NSURL URLWithString:@""] resultBlock:^(ALAsset *asset) {
+        NSLog(@"asset:%@",asset);
+        //成功返回ALAsset
+    } failureBlock:^(NSError *error) {
+        NSLog(@"error:%@",error);
+        //获取失败
+    }];
+    //通过URL地址获取相册ALAssetsGroup，该方法是异步的
+    [assetsLibrary groupForURL:[NSURL URLWithString:@""] resultBlock:^(ALAssetsGroup *group) {
+        //成功返回ALAssetsGroup
+        NSLog(@"group:%@",group);
+    } failureBlock:^(NSError *error) {
+        NSLog(@"error:%@",error);
+        //获取失败
+    }];
+    
+    NSMutableArray *arrayAssetsGroup=[[NSMutableArray alloc]init];
+    [assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupPhotoStream usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+        if (group) {
+            //group不为nil执行的操作，并将继续迭代下去
+            [arrayAssetsGroup addObject:group];
+            NSLog(@"group:%@",group);
+        }else{
+            //group为nil时执行的操作，并停止迭代，回到主线程
+//            [self performSelectorOnMainThread:@selector(reloadTableView) withObject:nil waitUntilDone:YES];
+        }
+    } failureBlock:^(NSError *error) {
+        //获取相册出错时，如该应用没有被授权访问相册时
+        NSLog(@"错误Error:%@",error);
+    }];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
