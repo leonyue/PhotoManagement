2f7e8fd3432347a4b94c2a5f24c25ac443a0d600
leon yue <4940748@qq.com>
2018-03-08 09:02:36 +0800

    Initial Commit

---
+++ PhotoManagement/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoManagement
+//
+//  Created by km on 2018/3/8.
+//  Copyright © 2018年 ly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
